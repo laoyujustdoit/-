d1c54e6fd5c70748e797a4b6c7c8d6bf7356fb71
laoyujustdoit <137618033+laoyujustdoit@users.noreply.github.com>
2023-06-27 16:02:23 +0800

    Add files via upload

---
+++ src/main/webapp/admin/src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+                return {
+            url : "http://localhost:8080/ssmx393r/",
+            name: "ssmx393r",
+            // 退出到首页链接
+            indexUrl: 'http://localhost:8080/ssmx393r/front/index.html'
+        };
+            },
+    getProjectName(){
+        return {
+            projectName: "高校实习实训管理系统"
+        } 
+    }
+}
+export default base
